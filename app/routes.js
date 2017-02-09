a7850ae1b81a859d449f800a659e6e25ea290128
Heet Vachhani <heet_vachhani@hotmail.com>
2017-02-08 18:43:24 -0600

    Test with greeting schema

--- app/routes.js
+++ app/routes.js
@@ -6,8 +6,22 @@ module.exports = function(app) {
 
 	// frontend routes =========================================================
 	// route to handle all angular requests
-	app.get('*', function(req, res) {
+	app.get('/', function(req, res) {
 		res.sendfile('./public/index.html');
 	});
+
+
+	var greetingSchema = mongoose.Schema({
+	  sentence: String
+	}); 
+	var Greeting= mongoose.model('Greeting', greetingSchema);
+
+	app.get('/test', function(req, res){
+  		Greeting.findOne(function (err, greeting) {
+    		res.send(greeting.sentence);
+  		});
+	});
+
+	
 
 };
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
+var http      = require('http');
 var express        = require('express');
-var app            = express();
 var mongoose       = require('mongoose');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
-
+var app            = express();
 // get db config
 var db = require('./config/db');
 var dbPath = db.url;
@@ -20,9 +20,22 @@ mongoose.connection.on('error', function(err){
   console.log('database connect error '+err);
 }); // mongoose.connection.on()
 
+
+var greetingSchema = mongoose.Schema({
+  sentence: String
+}); 
+var Greeting= mongoose.model('Greeting', greetingSchema);
+
 mongoose.connection.once('open', function() {
+	var greeting;
+  	Greeting.find( function(err, greetings){
+   if( !greetings ){     
+      greeting = new Greeting({ sentence: "Test" }); 
+      greeting.save();
+    } 
+  }); 
 	console.log("It worked!!");
-}
+});
 
 // get all data/stuff of the body (POST) parameters
 app.use(bodyParser.json()); // parse application/json 
@@ -31,6 +44,15 @@ app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-f
 
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
 app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
+
+app.use(function(err, req, res, next){
+  if (req.xhr) {
+    res.send(500, 'Something went wrong!');
+  }
+  else {
+    next(err);
+  }
+});
 
 // routes ==================================================
 require('./app/routes')(app); // pass our application into our routes
