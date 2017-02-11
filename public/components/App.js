b7a3cc665943988e4b2d763c45a585a282b0696d
Heet Vachhani <heet_vachhani@hotmail.com>
2017-02-11 15:39:47 -0600

    simple UI using Material-Ui

--- public/components/App.js
+++ public/components/App.js
@@ -1,8 +1,59 @@
-import React from 'react';
+import React, {Component} from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+const styles = {
+  floatingLabelStyle: {
+    color: '#00A1DF'
+  }
+};
 
-const App = () => (
-  <RaisedButton label="Default" />
-);
 
-export default App;
+export default class App extends Component {
+
+  render() {
+  	return (
+		<div>
+			<TextField
+		      floatingLabelText="First Name"
+		      floatingLabelFixed={true}
+		      floatingLabelStyle={styles.floatingLabelStyle}
+		    /><br />
+		    <TextField
+		      floatingLabelText="Last Name"
+		      floatingLabelFixed={true}
+		      floatingLabelStyle={styles.floatingLabelStyle}
+		    /><br />
+		    <TextField
+		      floatingLabelText="Company"
+		      floatingLabelFixed={true}
+		      floatingLabelStyle={styles.floatingLabelStyle}
+			/><br />
+		    <TextField
+		      floatingLabelText="Web Address"
+		      floatingLabelFixed={true}
+		      floatingLabelStyle={styles.floatingLabelStyle}
+		    /><br />
+		    <TextField
+		      floatingLabelText="Phone Number"
+		      floatingLabelFixed={true}
+		      floatingLabelStyle={styles.floatingLabelStyle}
+		    /><br />
+		    <SelectField
+		          floatingLabelText="Candy Specialty"
+		          floatingLabelStyle={styles.floatingLabelStyle}
+		          value={1}
+		          onChange={this.handleChange}
+		        >
+		          <MenuItem value={1} primaryText="Chocolate" />
+		          <MenuItem value={2} primaryText="Lollipops" />
+		          <MenuItem value={3} primaryText="Gum" />
+		          <MenuItem value={4} primaryText="Sour hard candies" />
+		    </SelectField><br />
+		  	<RaisedButton label="Submit" primary={true} />
+		</div>
+  	);
+  }
+}
+
--- public/main.js
+++ public/main.js
@@ -4,17 +4,24 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import App from './components/App';
 import reducers from './reducers';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 const store = createStoreWithMiddleware(reducers);
+const muiTheme = getMuiTheme({
+  fontFamily: {
+    fontFamily: 'proximanova-light'
+  }
+});
+
 
 ReactDOM.render(
  
   <Provider store={store}>
-    <MuiThemeProvider>
+    <MuiThemeProvider muiTheme={muiTheme}>
       <App />
   	</MuiThemeProvider>
   </Provider>, document.getElementById('app'));
